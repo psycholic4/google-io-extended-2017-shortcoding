5a8ad066f9cc134ca3ef7c483b9cd24b0a3408fb
Prince Kim <prince@vcnc.co.kr>
2017-07-07 14:29:00 +0900

    remove typealias

--- src/main/kotlin/com/rainist/ioextended/Main.kt
+++ src/main/kotlin/com/rainist/ioextended/Main.kt
@@ -1,1 +1,1 @@
-package com.rainist.ioextended;typealias L=List<Long>;fun solve(a:L):L =if(a.size<1)a else a.take(1)+solve(a-a[0]-a[0]*4/3)
+package com.rainist.ioextended;fun solve(a:List<Long>):List<Long> =if(a.size<1)a else a.take(1)+solve(a-a[0]-a[0]*4/3)
